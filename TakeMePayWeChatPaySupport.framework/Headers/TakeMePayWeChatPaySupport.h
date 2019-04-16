65cc7ba43f9ea3accaf8df2d993c17137284c7c8
lumiasaki <edward9941@gmail.com>
2019-04-16 16:36:07 +0900

    upload framework

---
+++ TakeMePayWeChatPaySupport.framework/Headers/TakeMePayWeChatPaySupport.h
@@ -0,0 +1,19 @@
+//
+//  TakeMePayWeChatPaySupport.h
+//  TakeMePayWeChatPaySupport
+//
+//  Created by tianren.zhu on 2019/4/15.
+//  Copyright © 2019 TakeMePay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TakeMePayWeChatPaySupport.
+FOUNDATION_EXPORT double TakeMePayWeChatPaySupportVersionNumber;
+
+//! Project version string for TakeMePayWeChatPaySupport.
+FOUNDATION_EXPORT const unsigned char TakeMePayWeChatPaySupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TakeMePayWeChatPaySupport/PublicHeader.h>
+
+
